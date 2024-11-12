eef2c84cf7d1a2d0b948eac3fd1fe94424fd662e
Aminadad <aminadad77@hotmail.com>
2024-11-12 13:19:01 -0500

    metodo a

--- abrir_archivo.py
+++ abrir_archivo.py
@@ -2,5 +2,6 @@ def leer_archivo(texto):
     file = open(texto,"r")
     leer = file.readlines()
     for i in leer:
-        passw = i.split("\n")
-        print(passw)
+        print(f"{i.replace('\n','')}")
+
+leer_archivo("contrasenas.txt")
--- archivos.py
+++ archivos.py
@@ -3,7 +3,7 @@ from funciones import cont_segura as fn
 
 mi_password = fn.new_password()
 
-file = open("contrasenias.txt","w")
+file = open("contrasenas.txt","a")
 file.write(mi_password)
 file.close()
 if len(mi_password)>0:
