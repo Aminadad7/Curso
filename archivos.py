bd9bcdbcd442597b30e015445d59ff7e1de9108b
Aminadad <aminadad77@hotmail.com>
2024-11-12 13:38:44 -0500

    metodo escribir

--- archivos.py
+++ archivos.py
@@ -1,13 +1,16 @@
 from funciones import cont_segura as fn
 
 
-mi_password = fn.new_password()
-
-file = open("contrasenas.txt","a")
-file.write(mi_password)
-file.close()
-if len(mi_password)>0:
-    print("Contrasena guardada")
+def escribir_archivo(texto):
+    mi_password = fn.new_password()
+    file = open(texto,"a")
+    file.write(mi_password)
+    file.close()
+    if len(mi_password)>0:
+        print("Contrasena guardada")
+        return mi_password
+    
+escribir_archivo("contrasenas.txt")
 
 
 
